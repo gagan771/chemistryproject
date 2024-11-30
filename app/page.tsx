6f074318d9fda2cceab7083159bfcccad3b09b11
Gagan Bhardwaj <gaganbhardwaj514@gmail.com>
2024-11-30 14:35:14 +0530

    Add Tailwind CSS configuration, utility functions, and update global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,30 @@
 // pages/index.tsx
-import Head from 'next/head';
-import Layout from '../comonents/Layout';
+"use client";
+import React from "react";
+import { BackgroundBeams } from "../components/ui/background-beams";
 
-const Home: React.FC = () => {
+export function BackgroundBeamsDemo() {
   return (
-    <>
-      <Head>
-        <title>My Next.js Project</title>
-        <meta name="description" content="Welcome to my Next.js project!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-        <div 
-          className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center" 
-          style={{ backgroundImage: "url('/background.jpg')" }}
-        >
-          <h1 className="text-5xl font-bold text-white">Welcome to My Project!</h1>
-          <p className="text-xl text-white mt-4">Explore the options in the navigation bar.</p>
-        </div>
-      
-    </>
+    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          Join the waitlist
+        </h1>
+        <p></p>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+          Welcome to MailJet, the best transactional email service on the web.
+          We provide reliable, scalable, and customizable email solutions for
+          your business. Whether you&apos;re sending order confirmations,
+          password reset emails, or promotional campaigns, MailJet has got you
+          covered.
+        </p>
+        <input
+          type="text"
+          placeholder="hi@manuarora.in"
+          className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+        />
+      </div>
+      <BackgroundBeams />
+    </div>
   );
-};
-
-export default Home;
+}
